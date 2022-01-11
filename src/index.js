9aa771c997ca81e5bc45a10fa8c67b5374c46e42
mcksarge <higgins.fred.s@gmail.com>
2022-01-10 19:27:57 -0800

    Working on like button

--- src/index.js
+++ src/index.js
@@ -9,47 +9,106 @@ document.addEventListener('DOMContentLoaded', () => { //DOM is loaded
     .then(quotes => {
         quotes.forEach(quote => {
             let li = document.createElement('li');
-            let likes = quote.likes[0]
-
+            li.classList.add('quote-card');
             li.innerHTML = `
-                <li class='quote-card'>
-                    <blockquote class="blockquote">
+                <blockquote class="blockquote">
                     <p class="mb-0">${quote.quote}</p>
                     <footer class="blockquote-footer">${quote.author}</footer>
                     <br>
-                    <button class='btn-success'>Likes: <span>${likes}</span></button>
+                    <button class='btn-success'>Likes: <span>0</span></button>
                     <button class='btn-danger'>Delete</button>
                 </blockquote>
-            </li>
             `
-   
-            quoteList.appendChild(li)
-        })
-
-
-        //Like Button
-        document.querySelector('.btn-success').addEventListener('click', (e) => {
-            console.log('You clicked like!')
-
-
-        })
-
-
-        //Delete Button
-        document.querySelector('.btn-danger').addEventListener('click', (e) => {
-            console.log('You clicked delete!')
-
-
-        })
-    });
-
-
+            
+            
+            //Like Button
+            let likeBtn = li.querySelector('.btn-success')
+            likeBtn.addEventListener('click', (e) => {
+                console.log('You clicked like!')
     
+            });
+
+            //Delete Button
+            let deleteBtn = li.querySelector('.btn-danger')
+            deleteBtn.addEventListener('click', (e) => {
+                console.log('You clicked delete!')
+                li.remove()
+            });
+
+            quoteList.appendChild(li)
+        });
+
+
+    });
 
 
 });
 
 
+//Handles Form
+let quoteForm = document.querySelector('#new-quote-form')
+let submitBtn = document.querySelector('.btn-primary')
+
+quoteForm.addEventListener('submit', handleSubmit);
 
 
+
+//Submit Button
+
+function handleSubmit(e){
+    e.preventDefault();
+    let quoteObj = {
+        quote: e.target.quote.value,
+        author: e.target.author.value
+    }
+    console.log(quoteObj)
+    renderQuote(quoteObj)
+    sendQuote(quoteObj)
+};
+
+function renderQuote(quote){
+    let li = document.createElement('li');
+    li.classList.add('quote-card');
+    li.innerHTML = `
+        <blockquote class="blockquote">
+            <p class="mb-0">${quote.quote}</p>
+            <footer class="blockquote-footer">${quote.author}</footer>
+            <br>
+            <button class='btn-success'>Likes: <span>${quote.likes}</span></button>
+            <button class='btn-danger'>Delete</button>
+        </blockquote>
+        `
+     //Like Button
+     let likeBtn = li.querySelector('.btn-success')
+     likeBtn.addEventListener('click', (e) => {
+         console.log('You clicked like!')
+
+     });
+
+      //Delete Button
+      let deleteBtn = li.querySelector('.btn-danger')
+      deleteBtn.addEventListener('click', (e) => {
+          console.log('You clicked delete!')
+          li.remove()
+      });
+
+      quoteList.appendChild(li)
+    
+};
+
+
+function sendQuote(quote){
+    fetch(`http://localhost:3000/quotes`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(quote)
+    })
+}
+
+
+function updateLikes(quote){
+    fetch
+}
 
